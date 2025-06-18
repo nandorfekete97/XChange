3c913c9c6f86d91913e2d04deb382eaca1d80953
Fekete Nándor <nandor.fekete@yahoo.com>
2025-06-18 10:01:05 +0200

    correction in App.jsx

--- xchange-frontend/src/App.jsx
+++ xchange-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import CurrencyConverterLayout from './Components/CurrencyConverterLayout';
+import CurrencyConverterLayout from './Components/Layout/CurrencyConverterLayout';
 
 function App() {
 
